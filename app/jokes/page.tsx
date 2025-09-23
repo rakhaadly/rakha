ce6fdd0f9014b4ae56475aa60e29d3f4c70e090c
rakhaadly <rakhairsyad22@gmail.com>
2025-09-23 08:30:04 +0700

    revisi

--- app/jokes/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function apaaja () {
-    return (
-        <div></div>
-    )
-}
--- userapi/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function UsersPage() {
-  return (
-    <div>
-      <h1>Users Page</h1>
-      <p>Ini adalah halaman utama /users</p>
-    </div>
-  );
-}
